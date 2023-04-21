b8e4243e60345fb6bc792af9016e77f54c5ef8c7
Rafael Reis <rafaeloliveira63@yahoo.com>
2023-04-21 20:40:35 -0300

    commit inicial

---
+++ src/main/java/br/com/backEndVendas/demo/BackEndVendasApplication.java
@@ -0,0 +1,12 @@
+package br.com.backEndVendas.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndVendasApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/backEndVendas/demo/BackEndVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.backEndVendas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
