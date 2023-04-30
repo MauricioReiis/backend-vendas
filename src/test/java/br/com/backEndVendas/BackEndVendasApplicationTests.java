757c0d3110a63a4b7d43c3b00c0cc1e0a3c83e08
leandromaga <leandro.2008_mg@hotmail.com>
2023-04-29 22:32:55 -0300

    teste

---
+++ src/test/java/br/com/backEndVendas/BackEndVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.backEndVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
