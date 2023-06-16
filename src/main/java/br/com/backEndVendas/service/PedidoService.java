76e56fd342488f66332863d891d7fd89d70c2688
Rafael Reis <rafaeloliveira63@yahoo.com>
2023-06-16 12:39:11 -0300

    resolvendo conflitos de dependencia

--- src/main/java/br/com/backEndVendas/service/PedidoService.java
+++ src/main/java/br/com/backEndVendas/service/PedidoService.java
@@ -145,7 +145,7 @@ public class PedidoService {
 
         notaVendaDao.save(notaVenda);
 
-        vonageApi(pedidoJson.getIdPedido(),pedidoJson.getIdCliente());
+//        vonageApi(pedidoJson.getIdPedido(),pedidoJson.getIdCliente());
 
         return 3;
     }
